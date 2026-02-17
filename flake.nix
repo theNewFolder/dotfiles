a35de1f66a7cbd5cb6ac65c29ae36cde84a317d9
dev <dev@tuxnix.local>
2026-02-17 17:28:00 +0400

    Enable home-manager file backups to avoid conflicts

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.dev = import ./home-manager/home.nix;
+            home-manager.backupFileExtension = "backup";
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments
             # to home.nix
